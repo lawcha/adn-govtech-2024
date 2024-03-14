d0b41c6ee7aaac8d014b907b707cb9ad31cfe8ba
Julien Marengo <julien.marengo@adnovum.ch>
2024-03-14 17:19:07 +0100

    Style error component

--- hackathon-frontend/src/app/components/question/question.component.ts
+++ hackathon-frontend/src/app/components/question/question.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { CardComponent } from '../card/card.component';
+import { ErrorComponent } from '../error/error.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-question',
   standalone: true,
   imports: [
-    CardComponent
+    CardComponent,
+    ErrorComponent,
+    NgIf
   ],
   templateUrl: './question.component.html',
   styleUrl: './question.component.scss'
