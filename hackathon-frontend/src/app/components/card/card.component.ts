0891ae7653dab74120c9c57cf294558cf4dcfc62
Lucie Schaffner <lucie.schaffner@adnovum.ch>
2024-03-14 16:52:03 +0100

    beautifuuuul card

--- hackathon-frontend/src/app/components/card/card.component.ts
+++ hackathon-frontend/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  @Input() image?: string;
+  @Input() answer?: string;
 
 	onClick() {
 
