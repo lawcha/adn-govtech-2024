4786ed89a11b659c8cc99c9c82a3fbcabffe95c3
kieran.schubert <schubert.kieran@gmail.com>
2024-03-14 16:00:07 +0100

    update imports, define request/response classes manually, add llm logic

--- src/llm/llm_api.py
+++ src/llm/llm_api.py
@@ -1,10 +1,20 @@
 import os
 from fastapi import FastAPI
-import langchain
+from langchain.chat_models import ChatOpenAI
+#from langchain.schema import SystemMessage, HumanMessage
+from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
 
 from dotenv import load_dotenv
 
-from ...src.request_schema import LLMRequest, LLMResponse
+#from ...src.request_schema import LLMRequest, LLMResponse
+
+from pydantic import BaseModel
+
+class LLMRequest(BaseModel):
+    query: str
+
+class LLMResponse(BaseModel):
+    response: str
 
 # load env variables
 load_dotenv()
@@ -16,6 +26,37 @@ app = FastAPI()
 @app.post("/llm", response_model = LLMResponse)
 def llm(request: LLMRequest) -> LLMResponse:
 
-    request.query = None
+    llm = ChatOpenAI(
+                    openai_api_key=OPENAI_API_KEY,
+                    model="gpt-3.5-turbo-0125",
+                    temperature=0,
+                    max_tokens=2048,
+                    streaming=False,
+                    model_kwargs={
+                        "top_p": 0.95,
+                      },
+                    )
 
-    return None
+    # create chat template
+    template = (
+        "You are a helpful assistant that extracts key information from legal documents. Your task is to simplify complex legal language and provide clear and concise explanations of legal concepts and key steps to undertake. NEVER omit important information or provide incorrect information, but ALWAYS try to simplify information as much as possible. If possible, provide a step-by-step explanation of tasks to undertake with bullet points, in markdown."
+    )
+
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_template = "{text}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
+
+    # get a chat completion from the formatted messages
+    llm_response = llm.invoke(
+        chat_prompt.format_prompt(
+            text=request.query
+        ).to_messages()
+    )
+
+    print(llm_response.content)
+
+    return {"response": llm_response.content}
