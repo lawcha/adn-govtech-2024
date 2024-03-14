89ff97881b134d99808cfaafdb388a6ddda20108
corsi <simon.corboz@adnovum.ch>
2024-03-14 16:12:17 +0100

    dockerfile works

--- src/main.py
+++ src/main.py
@@ -3,15 +3,7 @@ from fastapi import Depends, FastAPI
 #from .llm import admin
 #from .qa import items, users
 
-app = FastAPI(dependencies=[Depends(get_query_token)])
-
-app.include_router(
-    admin.router,
-    prefix="/admin",
-    tags=["admin"],
-    dependencies=[Depends(get_token_header)],
-    responses={418: {"description": "I'm a teapot"}},
-)
+app = FastAPI()
 @app.get("/")
 async def root():
-    return {"message": "Hello Bigger Applications!"}
+    return {"message": "hello"}
