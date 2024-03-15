18a9f77718a36ceef529ef58478e226f1ade702d
Julien Marengo <julien.marengo@adnovum.ch>
2024-03-15 14:22:50 +0100

    Prepare country dropdown

--- hackathon-frontend/src/app/components/question/question.component.ts
+++ hackathon-frontend/src/app/components/question/question.component.ts
@@ -4,7 +4,8 @@ import { ErrorComponent } from '../error/error.component';
 import { CommonModule, NgForOf, NgIf } from '@angular/common';
 import { QuestionWithAnswersModel } from '../../model/question-with-answers.model';
 import { SurveyStartedComponent } from '../survey-started/survey-started.component';
-import { AnswerModel } from '../../model/answer.model';
+import { AnswerModel, TypeEnum } from '../../model/answer.model';
+import { CountryDropdownComponent } from '../country-dropdown/country-dropdown.component';
 
 @Component({
 	selector: 'app-question',
@@ -15,7 +16,8 @@ import { AnswerModel } from '../../model/answer.model';
 		NgIf,
 		NgForOf,
 		CommonModule,
-		SurveyStartedComponent
+		SurveyStartedComponent,
+		CountryDropdownComponent
 	],
 	templateUrl: './question.component.html',
 	styleUrl: './question.component.scss'
@@ -43,5 +45,48 @@ export class QuestionComponent {
 
 	setSelectedAnswer($event: AnswerModel) {
 		this.selectedAnswer = $event;
+	}
+
+	setSelectedCountry(country: string) {
+		switch (country) {
+			case 'Switzerland':
+			case 'Germany':
+				this.selectedAnswer = {
+					level: 4,
+					question_id: 5,
+					answer: 'NO_RISK',
+					id: 10,
+					type: TypeEnum.COUNTRY
+				};
+				break;
+			case 'Thailand':
+				this.selectedAnswer = {
+					level: 4,
+					question_id: 5,
+					answer: 'HIGH_RISK',
+					id: 12,
+					type: TypeEnum.COUNTRY
+				};
+				break;
+			case 'USA':
+				this.selectedAnswer = {
+					level: 4,
+					question_id: 5,
+					answer: 'LOW_RISK',
+					id: 11,
+					type: TypeEnum.COUNTRY
+				};
+				break;
+			default:
+				this.selectedAnswer = {
+					level: 4,
+					question_id: 5,
+					answer: 'NO_RISK',
+					id: 10,
+					type: TypeEnum.COUNTRY
+				};
+				break;
+		}
+
 	}
 }
