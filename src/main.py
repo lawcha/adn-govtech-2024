b3ff278a03a4ebfcd4e7baeb14af7c6fb774b5e0
corsi <simon.corboz@adnovum.ch>
2024-03-15 13:46:28 +0100

    allow all origins

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .llm import llm_api
 from .qa import qa_service
@@ -7,6 +8,18 @@ app = FastAPI()
 app.include_router(llm_api.router)
 app.include_router(qa_service.router)
 app.include_router(content_api.router)
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {"message": "hello"}
