12eabd356fcaf7db8f43f4296d79606e6a0eae4d
Julien Marengo <julien.marengo@adnovum.ch>
2024-03-15 08:04:35 +0100

    Add check icon

--- hackathon-frontend/src/app/components/question/question.component.ts
+++ hackathon-frontend/src/app/components/question/question.component.ts
@@ -9,11 +9,12 @@ import { NgIf } from '@angular/common';
   imports: [
     CardComponent,
     ErrorComponent,
-    NgIf
+    NgIf,
   ],
   templateUrl: './question.component.html',
   styleUrl: './question.component.scss'
 })
 export class QuestionComponent {
   @Input() title?: string;
+  answered = false;
 }
