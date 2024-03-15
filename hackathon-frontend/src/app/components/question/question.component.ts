7667a4a18043f272b7403db18876e42d4d468684
Julien Marengo <julien.marengo@adnovum.ch>
2024-03-15 11:21:05 +0100

    Emit model with answer id when next question clicked

--- hackathon-frontend/src/app/components/question/question.component.ts
+++ hackathon-frontend/src/app/components/question/question.component.ts
@@ -1,25 +1,47 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { ErrorComponent } from '../error/error.component';
 import { CommonModule, NgForOf, NgIf } from '@angular/common';
 import { QuestionWithAnswersModel } from '../../model/question-with-answers.model';
 import { SurveyStartedComponent } from '../survey-started/survey-started.component';
+import { AnswerModel } from '../../model/answer.model';
 
 @Component({
-  selector: 'app-question',
-  standalone: true,
-  imports: [
-    CardComponent,
-    ErrorComponent,
-    NgIf,
-    NgForOf,
-    CommonModule,
-    SurveyStartedComponent
-  ],
-  templateUrl: './question.component.html',
-  styleUrl: './question.component.scss'
+	selector: 'app-question',
+	standalone: true,
+	imports: [
+		CardComponent,
+		ErrorComponent,
+		NgIf,
+		NgForOf,
+		CommonModule,
+		SurveyStartedComponent
+	],
+	templateUrl: './question.component.html',
+	styleUrl: './question.component.scss'
 })
 export class QuestionComponent {
-  @Input() questionWithAnswersModel?: QuestionWithAnswersModel;
-  answered = false;
+	@Input() questionWithAnswersModel?: QuestionWithAnswersModel;
+	@Output() onCompleted: EventEmitter<QuestionWithAnswersModel> = new EventEmitter<QuestionWithAnswersModel>();
+	selectedAnswer?: AnswerModel;
+	isErrorDisplayed = false;
+	isSubmitted = false;
+
+	onNextQuestionClick() {
+		if (this.selectedAnswer && this.questionWithAnswersModel) {
+			this.isErrorDisplayed = false;
+			this.isSubmitted = true;
+			this.onCompleted.emit({
+				...this.questionWithAnswersModel,
+				selectedAnswerId: this.selectedAnswer.id
+			});
+		}
+		else {
+			this.isErrorDisplayed = true;
+		}
+	}
+
+	setSelectedAnswer($event: AnswerModel) {
+		this.selectedAnswer = $event;
+	}
 }
