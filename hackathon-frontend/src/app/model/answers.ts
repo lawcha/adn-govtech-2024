b1c86965085d4ad4bf87f10b6fdc8885346f7ad6
Lucie Schaffner <lucie.schaffner@adnovum.ch>
2024-03-15 10:13:21 +0100

    correct ids from mock

--- hackathon-frontend/src/app/model/answers.ts
+++ hackathon-frontend/src/app/model/answers.ts
@@ -2,69 +2,69 @@ import { AnswerModel, TypeEnum } from './answer.model';
 
 export const answer1 : AnswerModel = {
   level: 0,
-  question_id: 11,
+  question_id: 0,
   type: TypeEnum.COUNTRY,
   answer: 'Yes, I live in Switzerland'
 }
 
 export const answer2 : AnswerModel = {
   level: 0,
-  question_id: 11,
+  question_id: 0,
   type: TypeEnum.COUNTRY,
   answer: 'No, I live somewhere else'
 }
 
 export const answer3 : AnswerModel = {
   level: 1,
-  question_id: 12,
+  question_id: 1,
   type: TypeEnum.COUNTRY,
   answer: 'I am going on holidays with my pet'
 }
 
 export const answer4 : AnswerModel = {
   level: 1,
-  question_id: 12,
+  question_id: 1,
   type: TypeEnum.COUNTRY,
   answer: 'I am coming back to Switzerland from holidays with my pet.'
 }
 
 export const answer5 : AnswerModel = {
   level: 1,
-  question_id: 12,
+  question_id: 2,
   type: TypeEnum.COUNTRY,
   answer: 'I am moving to Switzerland and want to bring my pet with me.'
 }
 
 export const answer6 : AnswerModel = {
   level: 1,
-  question_id: 12,
+  question_id: 2,
   type: TypeEnum.COUNTRY,
   answer: 'I am going to holidays in Switzerland with my pet.'
 }
 
 export const answer7 : AnswerModel = {
   level: 2,
-  question_id: 13,
+  question_id: 3,
   type: TypeEnum.COUNTRY,
   answer: 'Cat'
 }
 
 export const answer8 : AnswerModel = {
   level: 2,
-  question_id: 13,
+  question_id: 3,
   type: TypeEnum.COUNTRY,
   answer: 'Dog'
 }
 
 export const answer9 : AnswerModel = {
   level: 3,
-  question_id: 14,
+  question_id: 4,
   type: TypeEnum.COUNTRY,
   answer: 'Less than 7 months ago'
 }
 export const answer10 : AnswerModel = {
   level: 3,
-  question_id: 14,
+  question_id: 4,
   type: TypeEnum.COUNTRY,
   answer: 'More than 7 months ago'
 }
--- hackathon-frontend/src/app/model/questions.ts
+++ hackathon-frontend/src/app/model/questions.ts
@@ -2,25 +2,31 @@ import { QuestionModel } from './question.model';
 
 export const question1 : QuestionModel = {
   level: 0,
-  id: 11,
+  id: 0,
   question: 'Where do you live ?'
 }
 
 export const question2 : QuestionModel = {
   level: 1,
-  id: 12,
+  id: 1,
   question: 'Which reason applies to you ?'
 }
 
 export const question3 : QuestionModel = {
-  level: 1,
-  id: 13,
+  level: 3,
+  id: 2,
   question: 'What animal are you bringing with you ?'
 }
 
 export const question4 : QuestionModel = {
-  level: 1,
-  id: 13,
+  level: 4,
+  id: 3,
   question: 'When was your animal born ?'
+}
+
+export const question5 : QuestionModel = {
+  level: 5,
+  id: 4,
+  question: 'What country was the animal in ?'
 }
 
